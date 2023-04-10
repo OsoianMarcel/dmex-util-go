ecfaf9934b5918b64a309bc0a7a3f32b2f8e5aeb
Osoian Marcel <osoian.marcel.d@gmail.com>
2023-04-10 23:10:06 +0300

    add real test values

--- hasher/cancelorder_test.go
+++ hasher/cancelorder_test.go
@@ -11,13 +11,13 @@ import (
 func Test_CancelOrder(t *testing.T) {
 	cancelOrder := hasher.CancelOrder{
 		ContractAddr: common.HexToAddress("0xBE8f518B6515b16BCCb141496e2677F0e41faDDA"),
-		OrderHash:    common.FromHex("0x4606c86aaf7a0bfc52f04010fc09543c4c021cb56459e4f9fafb845cb6fae730"),
-		UserAddr:     common.HexToAddress("0x0000000000000000000000000000000000000000"),
-		Nonce:        big.NewInt(1),
+		OrderHash:    common.FromHex("0xe63b0f878c1da07c0f21ddcc0d04f7f489b5dcec16307260c596db597e3263d1"),
+		UserAddr:     common.HexToAddress("0x0A24BdE9D7618f9B745036FE0A7b57c7451ED724"),
+		Nonce:        big.NewInt(10086),
 	}
 	hash := cancelOrder.Keccak256Hash()
 
-	expectedHash := "0x2e0aee7f64a547734a84b4e28f66361818302d9ab22e74761431c815e5a887c5"
+	expectedHash := "0xb710a6de64a93f8e7389dbbc73724a8b252b66a4cf45bdacae449da9568c66e0"
 
 	if hash.Hex() != expectedHash {
 		t.Error("cancel order hash failed")
--- hasher/createorder_test.go
+++ hasher/createorder_test.go
@@ -11,17 +11,17 @@ import (
 func Test_CreateOrder(t *testing.T) {
 	createOrder := hasher.CreateOrder{
 		ContractAddr:        common.HexToAddress("0xBE8f518B6515b16BCCb141496e2677F0e41faDDA"),
-		FuturesContractHash: common.FromHex("0x4606c86aaf7a0bfc52f04010fc09543c4c021cb56459e4f9fafb845cb6fae730"),
-		UserAddr:            common.HexToAddress("0x0000000000000000000000000000000000000000"),
-		Amount:              big.NewInt(1),
-		Price:               big.NewInt(1),
+		FuturesContractHash: common.FromHex("0x8968e895fc5ca0159b20577972b0499a895a3cb8d467632213fbc170ed9857ba"),
+		UserAddr:            common.HexToAddress("0x0A24BdE9D7618f9B745036FE0A7b57c7451ED724"),
+		Amount:              big.NewInt(363077879),
+		Price:               big.NewInt(12175000000),
 		Side:                true,
-		Nonce:               big.NewInt(1),
+		Nonce:               big.NewInt(4874),
 		Leverage:            big.NewInt(1),
 	}
 	hash := createOrder.Keccak256Hash()
 
-	expectedHash := "0x44191d2a03fb3f724aeb71269af555eaba7a9f978f38a11949e5c839f4e869ec"
+	expectedHash := "0x860aabb66e2b17865e150c49a920cb66572b44921ddca90611133767a653bd7e"
 
 	if hash.Hex() != expectedHash {
 		t.Error("create order hash failed")
